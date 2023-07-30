52cc3c8eb8191d2d7e4e3f0f940674a8a3296c3d
GitHub Action <action@github.com>
2023-07-30 01:45:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/fairyfromalfeya/intellijrefoldplugin/MyAction.kt
+++ src/main/kotlin/com/github/fairyfromalfeya/intellijrefoldplugin/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fairyfromalfeya.intellijrefoldplugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
